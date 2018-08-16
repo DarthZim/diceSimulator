3b976497e82ca9c1244e91fcb1c2e7038e707d89
Kelvin Smith <29700078+smallgrasseater@users.noreply.github.com>
2018-08-16 10:50:18 -0500

    Create dice.py

---
+++ dice.py
@@ -0,0 +1,36 @@
+"""Dice Rolling game
+
+
+ 
+prompts for a guess
+evaluates and gives a response
+"""
+
+
+import random
+
+win=0
+loss=0
+def rollDice(win,loss):
+    random.seed()    // so the generated number is different in each instance
+    num = random.randrange(1,6)
+
+
+    guess = input('Guess a number in the range 1-6:\t')
+
+    if num==guess:
+        print('You have guessed correctly the answer is/t',num)
+        win+=1
+    else:
+        print('you have guessed incorrectly the correct answer is:\t', num)
+        loss+=1
+
+    choice = input(' would you like to play again: \n Y for yes:\t')
+    if choice == 'Y' or 'y':
+        rollDice(win,loss)
+    else:
+        print('you have won: ', win, 'games')
+        print('you have lost: ',loss, 'games')
+        return    
+
+rollDice(win,loss)
